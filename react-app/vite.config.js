f4560296e60ec399ebd5236333812f15e235a1b0
nadavilder <nadavilder@gmail.com>
2024-01-16 19:20:17 -0800

    deploy

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolvePath } from 'react-router-dom'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base:"/Story List",
+  resolve:{
+    alias:{
+      '@':resolvePath(__dirname,'./src'),
+    },
+  },
 })
